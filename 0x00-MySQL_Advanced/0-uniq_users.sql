f8735e85f37008f13ea105ffd981f38d3c51c56d
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-11 23:00:14 +0100

    Add the completed tasks: 0, 1, and 2

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create a table of users with given attributes
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+This directory contains all the task in the 0x00-MySQL_Advanced project.
