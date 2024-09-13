953207b3be8162fbce80bada35e867f47ff282ee
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-13 11:08:54 +0100

    Change replace to update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,5 +10,5 @@ def update_topics(mongo_collection, name, topics):
     """
     mongo_collection.update_one(
         {'name': name},
-        {'topics': topics}
+        {'$set': {'topics': topics}}
     )
