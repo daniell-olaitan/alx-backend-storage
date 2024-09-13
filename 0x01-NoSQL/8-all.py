f09dc0b96f5e159925680d8b60bd2d4dfe322536
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-13 10:24:54 +0100

    List all documents in a mongodb collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+"""
+Implement a function that lists all documents in a collection
+"""
+from pymongo.collection import Collection
+import typing as t
+
+def list_all(
+        mongo_collection: Collection
+    ) -> t.Optional[t.List[t.Dict[str, t.Any]]]:
+    """
+    List all documents in a collection
+    """
+    return list(mongo_collection.find({}))
