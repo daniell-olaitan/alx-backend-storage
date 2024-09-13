dee9ed56e57cf6c7734d58ede28ac1d50ad6a8f8
daniell-olaitan <daniell.olaitan@gmail.com>
2024-09-13 10:57:20 +0100

    Add the completed tasks: 9 and 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Implement function that changes all topics of
+a school document based on the name
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Change all topics of a school document based on the name
+    """
+    mongo_collection.replace_one(
+        {'name': name},
+        {'topics': topics}
+    )
